8766803703cd578b3d591465ea7e70eb0e662ec5
k.demchenko <k.demchenko@cfunalog.ru>
2025-09-02 19:21:46 +0300

    fix update_progress

--- aiogram_renderer/renderer.py
+++ aiogram_renderer/renderer.py
@@ -183,8 +183,11 @@ class Renderer:
         try:
             return await self.bot.edit_message_text(chat_id=chat_id, message_id=message_id, text=new_text,
                                                     reply_markup=keyboard)
-        except TelegramBadRequest:
-            return await self.bot.send_message(chat_id=chat_id, text=new_text, reply_markup=keyboard)
+        except TelegramBadRequest as e:
+            if "message not found" in str(e):
+                return await self.bot.send_message(chat_id=chat_id, text=new_text, reply_markup=keyboard)
+            else:
+                return e
 
     async def __render_media(self, file: File | FileBytes, data: dict[str, Any], text: str,
                              chat_id: int, message_id: int = None, mode: str = RenderMode.ANSWER,
